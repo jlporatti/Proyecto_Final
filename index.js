a2299c6b2794861b1123b63861f8f33674730f40
JLP <jlporatti@hotmail.com>
2026-06-29 21:35:32 -0300

    Versión 1.02 - Depurada para Vercel 2

--- index.js
+++ index.js
@@ -19,11 +19,6 @@ await initializeFirebase();
 //const PORT = process.env.PORT || 3000;
 
 // Middlewares globales
-// app.use(cors()); // Habilitamos CORS para peticiones de origen cruzado
-// app.use(bodyParser.json()); // Parseamos body en formato JSON
-// app.use(bodyParser.urlencoded({ extended: true })); // Parseamos URL-encoded bodies
-// app.use(requestLogger); // Logger de peticiones
-
 app.use(cors());
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
@@ -97,16 +92,16 @@ app.use(errorHandler);
 //   console.log('='.repeat(50));
 // });
 
-// // Manejamos los errores no capturados
-// process.on('unhandledRejection', (err) => {
-//   console.error('❌ Error no manejado:', err);
-//   process.exit(1);
-// });
+// Manejamos los errores no capturados
+process.on('unhandledRejection', (err) => {
+  console.error('❌ Error no manejado:', err);
+  process.exit(1);
+});
 
-// process.on('uncaughtException', (err) => {
-//   console.error('❌ Excepción no capturada:', err);
-//   process.exit(1);
-// });
+process.on('uncaughtException', (err) => {
+  console.error('❌ Excepción no capturada:', err);
+  process.exit(1);
+});
 
 
 export default app;
